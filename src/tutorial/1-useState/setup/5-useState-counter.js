2b8d3a805291efbf1bf618750d60e700cbb8a3ca
fizjo <sadfg@sdfd.pl>
2022-03-15 16:19:28 +0100

    UseState-counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  }
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //setValue(value + 1); -> jak kliknę 5 razy szybko to i tak doda mi tylko o 1 wartość
+      setValue((prevState) => { // -> jak kliknę szybko 5 razy to będzie mi liczył aż do 5ciu
+        return prevState +1; // nowa wartość
+      })
+    }, 2000)
+  }
+
+  return (
+    <>
+    <section style={{margin: '4rem 0'}}>
+      <h2> regular counter </h2>
+      <h1> {value} </h1>
+      <button className='btn' onClick={() => setValue (value -1)}> decrease </button>
+      <button className='btn'onClick={reset}> reset </button>
+      <button className='btn' onClick={() => setValue (value + 1)}> increase </button>
+    </section>
+    <section style={{margin: '4rem 0'}}>
+      <h2> more complex counter </h2>
+      <h1> {value} </h1>
+      <button className='btn' onClick={complexIncrease}> increase later </button>
+    </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
