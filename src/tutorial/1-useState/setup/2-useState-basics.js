5dd6d6faad115bb7bafc048f54873c07e6421f4f
fizjo <sadfg@sdfd.pl>
2022-03-15 14:15:13 +0100

    UseState - array

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -5,9 +5,9 @@ import React, { useState } from 'react';
 const UseStateBasics = () => {
   console.log(useState("hello"));
   // const value = useState(1)[0]; --> to jest wartość
-  // const handler = useState(1)[1]; --> to jest funkcja
-  // console.log("value: " + value, "handler: " + handler);
-  const[text, setText] = useState('random title!');
+  // const handler = useState(1)[1]; --> to jest funkcja 
+  // console.log("value: " + value, "handler: " + handler); - pierwszym elementem jest wartość a drugim elementem jest funkcja która kontroluje wartość
+  const[text, setText] = useState('random title!'); // to jest the  hook
 
   const handleClick = () => {
     if (text === 'random title'){
