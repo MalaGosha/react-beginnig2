3517adc65ae542a2a76c576e2b62ca968a5a408d
fizjo <sadfg@sdfd.pl>
2022-03-22 23:08:24 +0100

    learning useContext, prop-drilling, propTypes, custom hooks, reactRouter

--- src/tutorial/11-react-router/setup/Home.js
+++ src/tutorial/11-react-router/setup/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>this is main page</p>
+        </div>
+    );
 };
 
 export default Home;
